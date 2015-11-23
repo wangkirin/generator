3ed21bf65986d5b51097f70cc4fa6b9166cda1e7
Meaglith Ma <genedna@gmail.com>
2015-11-23 11:04:08 +0800

    Reformat and refactoring router.

--- router/router.go
+++ router/router.go
@@ -10,14 +10,22 @@ func SetRouters(m *macaron.Macaron) {
 	m.Group("/b1", func() {
 		m.Group("/build", func() {
 			m.Post("/", handler.Build)
-			m.Post("/adddeamon", handler.AddDeamon)
 
-			m.Delete("/deldeamon", handler.DelDeamon)
+			m.Post("/deamon", handler.AddDeamon)
+			m.Delete("/deamon", handler.DelDeamon)
+		})
 
-			m.Get("/log/:protocol/:id", handler.Log)
-			m.Get("/info", handler.GetInfo)
-			m.Get("/getalljobs", handler.GetAllJobs)
-			m.Get("/getalldeamon", handler.GetAllDeamons)
+		m.Group("/log", func() {
+			m.Get("/:protocol/:id", handler.log)
+		})
+
+		m.Group("/job", func() {
+			m.Get("/all", handler.GetAllJobs)
+		})
+
+		m.Group("/daemon", func() {
+			m.Get("/all", handler.GetAllDeamons)
+			m.Get("/info/:id", handler.GetInfo)
 		})
 	})
 }
